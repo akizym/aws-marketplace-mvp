a3ee650d3f58969ff98f449e21b55c92064f10e2
Andriy <andriy.kizym@gmail.com>
2025-05-08 12:54:20 +0200

    Initial commit

---
+++ src/Altusproj/AltusprojStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace Altusproj
+{
+    public class AltusprojStack : Stack
+    {
+        internal AltusprojStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
